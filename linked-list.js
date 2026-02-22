fa5b19f7288ed8fe1fc6ce744f35f00c54bc3dbd
max-ldx <242964282+max-ldx@users.noreply.github.com>
2026-02-22 17:53:18 +0100

    feat: add append

---
+++ linked-list.js
@@ -0,0 +1,53 @@
+class LinkedList {
+    #head = null;
+    #size = 0;
+
+    append(value) {
+        const node = new Node(value);
+
+        if (this.#head === null) {
+            this.#head = node;
+        } else {
+            let temp = this.#head;
+            while (temp.next !== null) {
+                temp = temp.next;
+            }
+
+            temp.next = node;
+        }
+
+        this.#size++;
+    }
+}
+
+class Node {
+    #value = null;
+    #next = null;
+
+    constructor(value = null, next = null) {
+        this.#value = value;
+        this.#next = next;
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    set value(value) {
+        this.#value = value;
+    }
+
+    get next() {
+        return this.#next;
+    }
+
+    set next(next) {
+        this.#next = next;
+    }
+}
+
+const l = new LinkedList();
+l.append(1);
+l.append(2);
+l.append(3);
+console.log(l)
